8264d9eaf605cf19fe68b03781b3a20d7b189200
Rahul Karasi <karasirahul1999@gmail.com>
2022-11-23 17:15:26 +0530

    updated code

--- src/components/BuyModal.js
+++ src/components/BuyModal.js
@@ -4,72 +4,80 @@ import {
   TouchableOpacity,
   StyleSheet,
   TextInput,
+  SafeAreaView,
 } from 'react-native';
 import React, {useState} from 'react';
-import { useDispatch} from 'react-redux';
-import { buyCrypto } from '../redux/actions/getCryptoDataAction';
+import {useDispatch} from 'react-redux';
+import {buyCrypto} from '../redux/actions/getCryptoDataAction';
+import HeaderBar from './HeaderBar';
 
-const BuyModal = (props) => {
+const BuyModal = props => {
   const [inputText, setInputText] = useState('0');
   const dispatch = useDispatch();
 
   const handlePress = () => {
-    props.setModalVisiblity(false)
+    props.setModalVisiblity(false);
     dispatch(buyCrypto(props.modalData, inputText));
-  }
+  };
 
   return (
-    <View style={styles.modalContainer}>
-        <Text style={styles.inputHeader} >ENTER AMOUNT</Text>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={text => setInputText(text)}
-        placeholder='0'
-        placeholderTextColor={'rgb(124,142,161)'}
-        maxLength={7}
-        keyboardType='decimal-pad'
-      />
-      <TouchableOpacity style={styles.buyNowBtn} onPress={handlePress} >
-        <Text style={styles.buyNowBtnTxt} >BUY</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <HeaderBar />
+      <View style={styles.modalContainer}>
+        <Text style={styles.inputHeader}>ENTER AMOUNT</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={text => setInputText(text)}
+          placeholder="0"
+          placeholderTextColor={'rgb(124,142,161)'}
+          maxLength={7}
+          keyboardType="decimal-pad"
+        />
+        <TouchableOpacity style={styles.buyNowBtn} onPress={handlePress}>
+          <Text style={styles.buyNowBtnTxt}>BUY</Text>
+        </TouchableOpacity>
+        <View style={styles.empty} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   modalContainer: {
-    height: 662,
+    height: '100%',
     width: '100%',
     backgroundColor: 'rgb(38,52,69)',
-    position: 'absolute',
-    top: 104,
-    alignItems:'center',
-    justifyContent:'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   textInput: {
     height: 60,
-    // width:80,
-    // backgroundColor:'red',
-    fontSize:40,
-    fontWeight:'600',
-    color:'rgb(124,142,161)',
-    borderBottomWidth:1,
+    fontSize: 40,
+    fontWeight: '600',
+    color: 'rgb(124,142,161)',
+    borderBottomWidth: 1,
     borderBottomColor: 'rgb(124,142,161)',
-    marginVertical:10
+    marginVertical: 10,
   },
-  inputHeader:{
+  inputHeader: {
     color: 'rgb(124,142,161)',
   },
-  buyNowBtn:{
-    backgroundColor:'rgb(82,175,92)',
-    height:40,
-    width:120,
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:5
+  buyNowBtn: {
+    backgroundColor: 'rgb(82,175,92)',
+    height: 40,
+    width: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
   },
-  buyNowBtnTxt:{
-    color:'white'
+  buyNowBtnTxt: {
+    color: 'white',
+  },
+  empty:{
+    height:100
   }
 });
 
